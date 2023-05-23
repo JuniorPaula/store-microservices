95d3531ee17a5e4add5ea69a0e5666e0441ce834
Junior Paula <luke.junnior@icloud.com>
2023-05-23 19:06:19 -0300

    #payment implemnts method to notify payment order througth the queue

--- payment/main.go
+++ payment/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"payment/queue"
+	"time"
+
+	"github.com/streadway/amqp"
+)
+
+type Order struct {
+	UUID      string    `json:"uuid"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Phone     string    `json:"phone"`
+	ProductID string    `json:"product_id"`
+	Status    string    `json:"status"`
+	CreatedAt time.Time `json:"created_at,string"`
+}
+
+func notifyPaymentOrder(order Order, ch *amqp.Channel) {
+	json, _ := json.Marshal(order)
+	queue.Notify(json, "payment_ex", "", ch)
+
+	fmt.Println(string(json))
+}
 
 func main() {
-	fmt.Print("Hello, payment!!!")
+	in := make(chan []byte)
+
+	connection := queue.Connect()
+	queue.StartConsumer("order_queue", connection, in)
+
+	var order Order
+	for payload := range in {
+		json.Unmarshal(payload, &order)
+		order.Status = "approved"
+
+		notifyPaymentOrder(order, connection)
+	}
+
 }
