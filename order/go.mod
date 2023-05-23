abb4c507fb173a31a75cb5698228755d9a2b2394
Junior Paula <luke.junnior@icloud.com>
2023-05-23 08:46:24 -0300

    #order implements method to create a new order

--- order/go.mod
+++ order/go.mod
@@ -3,3 +3,8 @@ module order
 go 1.19
 
 require github.com/streadway/amqp v1.0.0
+
+require (
+	github.com/go-redis/redis/v7 v7.4.1 // indirect
+	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
+)
--- order/main.go
+++ order/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"order/queue"
 	"os"
+	"time"
+
+	uuid "github.com/nu7hatch/gouuid"
 )
 
 type Product struct {
@@ -13,19 +19,48 @@ type Product struct {
 }
 
 type Order struct {
-	UUID      string `json:"uuid"`
-	Name      string `json:"name"`
-	Email     string `json:"email"`
-	Phone     string `json:"phone"`
-	ProductID string `json:"product_id"`
-	Status    string `json:"status"`
-	CreatedAt string `json:"created_at,string"`
+	UUID      string    `json:"uuid"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Phone     string    `json:"phone"`
+	ProductID string    `json:"product_id"`
+	Status    string    `json:"status"`
+	CreatedAt time.Time `json:"created_at,string"`
 }
 
 var productURL string
 
 func init() {
 	productURL = os.Getenv("PRODUCT_URL")
+}
+
+func createOrder(payload []byte) {
+	var order Order
+	json.Unmarshal(payload, &order)
+
+	id, _ := uuid.NewV4()
+	order.UUID = id.String()
+	order.Status = "pending"
+	order.CreatedAt = time.Now()
+
+	saveOrder(order)
+}
+
+func saveOrder(order Order) {
+
+}
+
+func getProductByID(id string) Product {
+	response, err := http.Get(productURL + "/products/" + id)
+	if err != nil {
+		fmt.Printf("The HTTP request failed with error: %s\n", err)
+	}
+
+	data, _ := io.ReadAll(response.Body)
+
+	var product Product
+	json.Unmarshal(data, &product)
+	return product
 }
 
 func main() {
@@ -35,6 +70,7 @@ func main() {
 	queue.StartConsumer(connection, in)
 
 	for payload := range in {
+		createOrder(payload)
 		fmt.Println(string(payload))
 	}
 }
